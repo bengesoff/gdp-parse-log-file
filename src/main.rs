2ff7b8409ec6277d1d8a6a334f54fd585ed0e98e
Ben Gesoff <ben.gesoff@gmail.com>
2019-01-04 23:38:37 +0000

    Implement full parser and test on data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 #[macro_use]
 extern crate nom;
 
+use std::fs;
+
 mod parser;
 
 fn main() {
-    let header = parser::parse_transmission_header("Transmission ID: 45, Sent, Time(secs): 123453453\n--0");
-    println!("{:?}",header);
+    // Load the file into a &str
+    let file_contents = fs::read_to_string("../Testbed_Log.log").unwrap();
+    // Run the nom parser over the string
+    let transmissions = parser::get_transmissions(&file_contents);
+    // Count the total number of attempts
+    let attempts = transmissions.iter().fold(0, |acc, t| acc + t.attempts.len());
+    println!("Got {} transmissions. There were {} attempts which means that the average number of attempts per transmission was {}.", transmissions.len(), attempts, attempts as f64/transmissions.len() as f64);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -7,6 +7,12 @@ pub enum Direction {
 }
 
 #[derive(Debug,PartialEq)]
+pub struct Transmission {
+    pub header: Header,
+    pub attempts: Vec<Attempt>,
+}
+
+#[derive(Debug,PartialEq)]
 pub struct Header {
     pub id: u32,
     pub direction: Direction,
@@ -14,16 +20,19 @@ pub struct Header {
 
 #[derive(Debug,PartialEq)]
 pub struct Attempt {
-    pub corrupted: &str,
-    pub uncorrupted: &str,
+    pub attempt_number: u32,
+    pub corrupted: u32,
+    pub uncorrupted: u32,
     pub error: bool,
     pub detected_error: bool,
 }
 
-pub fn parse_transmission_header(line: &str) -> Header {
-    match transmission_header(line) {
-        Ok((_, result)) => result,
-        Err(_) => panic!("Failed")
+/// Parse the input string and get a vector of Transmission structs back
+pub fn get_transmissions(file: &str) -> Vec<Transmission> {
+    let res = many_transmissions(file);
+    match res {
+        Ok((_, all_transmissions)) => all_transmissions,
+        Err(e) => panic!("parse error: {:?}", e)
     }
 }
 
@@ -31,19 +40,78 @@ named!(transmission_header<&str, Header>,
    do_parse!(
        tag!("Transmission ID: ") >>
        id: map_res!(digit1, from_int) >>
-       comma >>
+       comma_space >>
        direction: map_res!(alt!(tag!("Sent") | tag!("Received")), get_direction) >>
-       comma >>
+       comma_space >>
        many1!(not_line_ending) >>
-       tag!("\n") >>
+       line_ending >> 
        (Header {id, direction})
    )
 );
 
-named!(comma<&str, &str>, tag!(", "));
+named!(transmission_attempt<&str, Attempt>,
+    do_parse!(
+        tag!("--") >>
+        attempt_number: hex_packet >>
+        comma >>
+        corrupted: hex_packet >>
+        comma >>
+        uncorrupted: hex_packet >>
+        comma >>
+        error: map_res!(digit, to_bool) >>
+        comma >>
+        detected_error: map_res!(digit, to_bool) >>
+        opt!(line_ending) >>
+        (Attempt {
+            attempt_number,
+            corrupted,
+            uncorrupted,
+            error,
+            detected_error
+        })
+    )
+);
+
+named!(transmission<&str, Transmission>,
+    do_parse!(
+        header: transmission_header >>
+        attempts: many0!(transmission_attempt) >>
+        (Transmission {
+            header,
+            attempts
+        })
+    )
+);
+
+named!(many_transmissions<&str, Vec<Transmission>>,
+   many1!(transmission)
+);
+
+named!(hex_packet<&str, u32>,
+    map_res!(take_while_m_n!(1,6,is_hex_digit), from_hex)
+);
+
+named!(comma_space<&str, &str>, tag!(", "));
+named!(comma<&str, &str>, tag!(","));
+
+fn is_hex_digit(c: char) -> bool {
+  c.is_digit(16)
+}
 
 fn from_int(input: &str) -> Result<u32, std::num::ParseIntError> {
     u32::from_str_radix(input, 10)
+}
+
+fn from_hex(input: &str) -> Result<u32, std::num::ParseIntError> {
+    u32::from_str_radix(input, 16)
+}
+
+fn to_bool(input: &str) -> Result<bool, u32> {
+    match input {
+        "0" => Ok(false),
+        "1" => Ok(true),
+        _ => Err(0)
+    }
 }
 
 fn get_direction(input: &str) -> Result<Direction, u32> {
@@ -59,6 +127,41 @@ fn parse_header() {
     assert_eq!(transmission_header("Transmission ID: 45, Sent, Time(secs): 123453453\n--0"), Ok(("--0", Header {
         id: 45,
         direction: Direction::Sent,
+    })));
+}
+
+#[test]
+fn parse_attempt() {
+    assert_eq!(transmission_attempt("--0,17a883,17a983,0,0\n"), Ok(("", Attempt {
+        attempt_number: 0,
+        corrupted: 0x17a883,
+        uncorrupted: 0x17a983,
+        error: false,
+        detected_error: false
+    })));
+}
+
+#[test]
+fn parse_transmission() {
+    let test = "Transmission ID: 45, Sent, Time(secs): 123453453\n--0,17a883,17a983,0,0\n--0,17a883,17a983,0,0\n\n\n";
+    assert_eq!(transmission(test), Ok(("\n\n", Transmission {
+        header: Header {
+            id: 45,
+            direction: Direction::Sent,
+        },
+        attempts: vec![Attempt {
+            attempt_number: 0,
+            corrupted: 0x17a883,
+            uncorrupted: 0x17a983,
+            error: false,
+            detected_error: false
+        }, Attempt {
+            attempt_number: 0,
+            corrupted: 0x17a883,
+            uncorrupted: 0x17a983,
+            error: false,
+            detected_error: false
+        }]
     })));
 }
 
